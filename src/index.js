0f2a700b41ee83c126bd5b2e576e858e5f140ea5
Arnaud Babol <arnaud@babol.me>
2018-04-07 22:35:58 +0200

    Add queue

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import loginToNike from './loginToNike';
 import bodyParser from 'body-parser';
+import queue from 'express-queue';
 
 const app = express();
 const jsonParser = bodyParser.json();
 
 app.use(bodyParser.json());
 app.get('/', (req, res) => res.json('Hello Nike!'));
+app.use('/login', queue({ activeLimit: 1, queuedLimit: -1 }));
 app.post('/login', async (req, res) => {
   const { login, password } = req.body;
 
